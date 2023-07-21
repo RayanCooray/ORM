0d1053229aef6345f859139f90af6febe48354c2
rayan <rayancooray123456@gmail.com>
2023-07-21 11:48:06 +0530

    making tables

--- src/main/java/FactoryConfiguration.java
+++ src/main/java/FactoryConfiguration.java
@@ -1,3 +1,4 @@
+import entity.Customer;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -9,7 +10,7 @@ public class FactoryConfiguration {
 
     private FactoryConfiguration(){
        Configuration configuration = new Configuration().
-               configure();
+               configure().addAnnotatedClass(Customer.class);
 
        sessionFactory = configuration.buildSessionFactory();
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,16 @@
+import entity.Customer;
+import org.hibernate.Session;
+
 public class Main {
     public static void main(String[] args)  {
-        System.out.println("Hello World");
+        Customer customer = new Customer();
+        customer.setId("200531003778");
+        customer.setName("Rayan");
+        customer.setAddress("Kalutara");
 
+        Session session = FactoryConfiguration.getInstance().getSession();
+        session.persist(customer);
+        //session.beginTransaction();
+        session.close();
     }
 }
